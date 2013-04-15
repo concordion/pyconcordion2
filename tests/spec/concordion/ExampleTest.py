56e4dae5e79559b3f4cc9f17e9fc97e69a2d6c8c
John Jiang <johnjiang101@gmail.com>
2013-04-15 22:01:10 +1000

    #3 Setup exampletest

---
+++ tests/spec/concordion/ExampleTest.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from pyconcordion2 import ConcordionTestCase
+from test_rig import TestRig
+
+
+class ExampleTest(ConcordionTestCase):
+    def process(self, html):
+        result = TestRig(fixture=self).process_html(html)
+        return "success" if result.has_succeeded() else "failure"
+
+    def greeting(self):
+        return "Hello World!"
