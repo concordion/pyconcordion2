4b07abcc85e2b5e775d163f3bbdb907f1e1ec76d
John Jiang <johnjiang101@gmail.com>
2013-04-22 23:05:02 +1000

    Fixed bug with run_test

--- pyconcordion2/tests/spec/run_test.py
+++ pyconcordion2/tests/spec/run_test.py
@@ -1,11 +1,10 @@
 import os
 import sys
 import unittest
-from IndexTest import IndexTest
 
 sys.path.insert(0, os.path.dirname(__file__))
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(2, os.path.join(os.path.dirname(__file__), "..", ".."))
-sys.path.insert(2, os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+sys.path.insert(3, os.path.join(os.path.dirname(__file__), "..", ".."))
 
+from IndexTest import IndexTest
 unittest.TextTestRunner().run(IndexTest())
