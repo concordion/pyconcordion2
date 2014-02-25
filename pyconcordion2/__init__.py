80cf78cc26b226bd9660d9bc6b8d795498e84ef3
John Jiang <johnjiang101@gmail.com>
2014-02-26 00:50:02 +1100

    Minor formatting

--- pyconcordion2/__init__.py
+++ pyconcordion2/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 from base import ConcordionTestCase
 
-#http://semver.org/
 VERSION = (0, 13, 0)
-__version__ = ".".join(map(str,VERSION))
+__version__ = ".".join(map(str, VERSION))
